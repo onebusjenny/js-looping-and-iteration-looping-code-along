cb10433500909d3eb429d5bb327663173359251c
Jenny <jchudesign0930@gmail.com>
2020-03-06 00:00:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// function writeCards(cards){
+//     for (let i = 0; i < cards.length; i++) {
+//         console.log(`Thank you, ${cards[i]} ,for the wonderful birthday gift!`)
+//     }
+//     return cards;
+// }
+// writeCards(cards);
+function writeCards(cards, giftType) {
+    let strings = [];
+    for (let i = 0; i < cards.length; i++) {
+       strings.push(`Thank you, ${cards[i]}, for the wonderful ${giftType} gift!`);
+    }
+    return strings;
+ }
+
+ function countDown(numbers){
+     let i = 11
+    while (i > 0){
+        console.log(numbers);
+        i--;
+    }
+
+ }
